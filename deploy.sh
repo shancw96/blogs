d00ed2a59965fb68cc629ccf7ef99cb0096afdfb
wu_sc <shancw1996@gmail.com>
2021-07-24 19:07:29 +0800

    docker deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ echo ">> start pushing image to 192.168.193.72:5000"
 docker push 192.168.193.72:5000/blog:test
 echo ">> image push success"
 echo ">> sync code to github and trigger git hook!"
-echo "auto deploy" > deploy.log
+echo "auto deploy" > deploy.tag
 git add . && git commit -m "docker deploy"
 git push origin master
