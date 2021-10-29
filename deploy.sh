367dc337b71aa09624dcf01a64758a5f8f8feaee
shanCW <shancw1996@gmail.com>
2021-10-29 10:39:36 +0800

    docker auto deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,30 @@
-echo ">> start building image..."
+highlight='\033[0;31m'
+echo -e "${highlight}---------------------------------------------"
+date
+echo building local static file...
+echo -e "${highlight}---------------------------------------------"
+
+npm run build
+echo -e "${highlight}---------------------------------------------"
+date
+echo building docker image...
+echo -e "${highlight}---------------------------------------------"
 docker rmi --force 192.168.193.72:5000/blog:latest
 docker build . -t 192.168.193.72:5000/blog:latest --no-cache
-echo "---------------------------"
-echo ">> start pushing image to 192.168.193.72:5000"
+echo -e "${highlight}---------------------------------------------"
+date
+echo pushing docker image...
+echo -e "${highlight}---------------------------------------------"
+
 docker push 192.168.193.72:5000/blog:latest
-echo ">> image push success"
-echo ">> sync code to github and trigger git hook!"
-echo "auto deploy" >> deploy.tag
-git add . && git commit -m "docker deploy"
+echo -e "${highlight}---------------------------------------------"
+date
+echo sync to github
+echo -e "${highlight}---------------------------------------------"
+git add . && git commit -m "docker auto deploy"
 git push origin master
-exit 0;
+echo -e "${highlight}---------------------------------------------"
+date
+echo deploy success!
+echo -e "${highlight}---------------------------------------------"
+exit 0
