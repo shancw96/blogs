34d4c34e9f8b91efc642fd2385767cc0cb308ad3
shanCW <shancw1996@gmail.com>
2021-11-03 09:35:54 +0800

    react hooks implement

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,14 @@ echo "${highlight}---------------------------------------------${NC}"
 date
 echo sync to github
 echo "${highlight}---------------------------------------------${NC}"
-git add . && git commit -m "docker auto deploy"
+time=$(date "+%Y-%m-%d %H:%M:%S")
+echo "please input commit message"
+read message
+git add .
+git commit -m "${message}"
+echo "auto deploy at:${time}" >> deploy.tag
+git add .
+git commit -m "auto deploy at ${time}"
 git push origin master
 echo "${highlight}---------------------------------------------${NC}"
 date
