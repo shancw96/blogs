3e29b906aff8c8a824eafdab42498aba2b4827d6
shanCW <shancw1996@gmail.com>
2021-12-16 17:55:12 +0800

    linux

--- deploy.sh
+++ deploy.sh
@@ -14,14 +14,14 @@ echo "${highlight}---------------------------------------------${NC}"
 date
 echo building docker image...
 echo "${highlight}---------------------------------------------${NC}"
-docker rmi --force 10.14.145.72:5000/blog:latest
-docker build . -t 10.14.145.72:5000/blog:latest --no-cache
+docker rmi --force 8.210.206.194:5000/blog:v1
+docker build . -t 8.210.206.194:5000/blog:v1 --no-cache
 echo "${highlight}---------------------------------------------${NC}"
 date
 echo pushing docker image...
 echo "${highlight}---------------------------------------------${NC}"
 
-docker push 10.14.145.72:5000/blog:latest
+docker push 8.210.206.194:5000/blog:v1
 echo "${highlight}---------------------------------------------${NC}"
 date
 echo sync to github
