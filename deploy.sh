61288b9605a6b8882be13de5291b31399d1a6e5e
shanCW <shancw1996@gmail.com>
2022-04-16 12:20:33 +0800

    fix: 还原docker部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,113 @@
 highlight='\033[0;31m'
 NC='\033[0m'
-echo "${highlight}----------------building local file...   -----------------------------${NC}"
+echo "${highlight}---------------------------------------------${NC}"
+
+echo "${highlight}---------------------------------------------${NC}"
+echo "${highlight}---------------------------------------------${NC}"
+date
+echo building local static file...
+echo "${highlight}---------------------------------------------${NC}"
+
 npm run build
-echo "${highlight}----------------building docker image...  -----------------------------${NC}"
-image_name=127.0.0.1:5000/blog:v1
-docker rm -f blog
-docker rmi $image_name
-docker build . -t $image_name --no-cache
-echo "${highlight}----------------run docker image...  -----------------------------${NC}"
-docker run -d -t -p 80:80 --name=blog --restart=always $image_name
+echo "${highlight}---------------------------------------------${NC}"
+date
+echo building docker image...
+echo "${highlight}---------------------------------------------${NC}"
+docker rmi --force 11.11.111.1:5000/blog:v1
+docker build . -t 11.11.111.1:5000/blog:v1 --no-cache
+echo "${highlight}---------------------------------------------${NC}"
+date
+echo pushing docker image...
+echo "${highlight}---------------------------------------------${NC}"
+
+docker push 11.11.111.1:5000/blog:v1
+echo "${highlight}---------------------------------------------${NC}"
+ssh shancw@serial.limiaomiao.site -p 11122 "sh /home/shancw/project/blog.sh"
+
+# date
+# echo sync to github
+# echo setting http proxy to http://127.0.0.1:7890
+# export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+# echo "${highlight}---------------------------------------------${NC}"
+# time=$(date "+%Y-%m-%d %H:%M:%S")
+# echo ">>> please input your commit message !"
+# read message
+# git add .
+# git commit -m "${message}"
+# echo "${highlight}---------------------------------------------${NC}"
+
+
+# docker create \
+#   --name=jackett \
+#   -e PUID=1000 \
+#   -e PGID=1000 \
+#   -e TZ=Asia/Shanghai \
+#   -p 9117:9117 \
+#   -v /home/shancw/data/jackett:/config \
+#   --restart unless-stopped \
+#   linuxserver/jackett
+
+# docker create \
+#   --name=sonarr \
+#   -e PUID=1000 \
+#   -e PGID=1000 \
+#   -e TZ=Asia/Shanghai \
+#   -p 8989:8989 \
+#   -v /home/shancw/data/sonnar:/config \
+#   -v /home/shancw/data/media:/media \
+#   --restart unless-stopped \
+#   linuxserver/sonarr:latest
+
+#   docker create  \
+#     --name=qbittorrentee  \
+#     -e WEBUIPORT=8080  \
+#     -e PUID=1026 \
+#     -e PGID=100 \
+#     -e TZ=Asia/Shanghai \
+#     -p 6881:6881  \
+#     -p 6881:6881/udp  \
+#     -p 8080:8080  \
+#     -v /home/shancw/data/qbittorrent:/config  \
+#     -v /home/shancw/data/media:/downloads  \
+#     --restart unless-stopped  \
+#     superng6/qbittorrentee:latest
+
+# version: "2.3"
+# services:
+#   emby:
+#     image: emby/embyserver
+#     container_name: embyserver
+#     runtime: nvidia # Expose NVIDIA GPUs
+#     network_mode: host # Enable DLNA and Wake-on-Lan
+#     environment:
+#       - UID=1000 # The UID to run emby as (default: 2)
+#       - GID=100 # The GID to run emby as (default 2)
+#       - GIDLIST=100 # A comma-separated list of additional GIDs to run emby as (default: 2)
+#     volumes:
+#       - /home/shancw/data/emby:/config # Configuration directory
+#       - /home/shancw/data/media:/mnt/share1 # Media directory
+#       - /home/shancw/data/media:/mnt/share2 # Media directory
+#     ports:
+#       - 8096:8096 # HTTP port
+#       - 8920:8920 # HTTPS port
+#     devices:
+#       - /dev/dri:/dev/dri # VAAPI/NVDEC/NVENC render nodes
+#       - /dev/vchiq:/dev/vchiq # MMAL/OMX on Raspberry Pi
+#     restart: unless-stopped
+# version: "3"
+# services:
+#   chinesesubfinder:
+#     image: allanpk716/chinesesubfinder:latest
+#     volumes:
+#       - /home/shancw/data/chinesesubfinder/SubFixCache:/app/cache
+#       - /home/shancw/data/chinesesubfinder:/config
+
+#       - /home/shancw/data/media:/media
+#     ports:
+#       - "19035:19035"
+#     environment:
+#       - PUID=1026
+#       - PGID=100
+#       - TZ=Asia/Shanghai
+#     restart: unless-stopped
+#   export https_proxy=http://192.168.5.233:7890 http_proxy=http://192.168.5.233:7890 all_proxy=socks5://192.168.5.233:7890
